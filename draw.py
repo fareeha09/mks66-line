bf77997627a2472e3beb25d9ebe01fe8e08c7728
fareeha09 <ftabassum@stuy.edu>
2019-02-06 15:36:37 -0500

    second

--- draw.py
+++ draw.py
@@ -1,4 +1,7 @@
 from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
-    pass
+    slope = (y1 - y0)/(x1 - x0)
+    if (slope == 0){
+        for i 
+        plot(screen, color, x0+i, y0)}
--- main.py
+++ main.py
@@ -6,3 +6,6 @@ color = [ 0, 255, 0 ]
 
 display(screen)
 save_extension(screen, 'img.png')
+
+#start (0,0); end (10,9)
+draw_line(0 , 0 , 10, 9, screen, color)
