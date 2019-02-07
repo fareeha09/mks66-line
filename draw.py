c7afa9e523c117e9c3886fb866a459896c8f04c8
fareeha09 <ftabassum@stuy.edu>
2019-02-07 11:53:28 -0500

    :|

--- draw.py
+++ draw.py
@@ -1,7 +1,15 @@
 from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
-    slope = (y1 - y0)/(x1 - x0)
-    if (slope == 0){
-        for i 
-        plot(screen, color, x0+i, y0)}
+    #if slope is undefined
+    if ((x1-x0) == 0):
+        for i in range(y1-y0):
+            plot(screen, color, x0, y0+i)
+    else:
+        slope = (y1 - y0)/(x1 - x0)
+        
+        #if slope is 0
+        if (slope == 0):
+            for i in range(x1-x0):
+                plot(screen, color, x0+i, y0)
+    
