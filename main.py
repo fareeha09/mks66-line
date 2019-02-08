a3eb40383842781c92a79845df3c77fc7dbc2a04
fareeha09 <ftabassum@stuy.edu>
2019-02-08 15:33:12 -0500

    lol

--- main.py
+++ main.py
@@ -7,5 +7,28 @@ color = [ 0, 255, 0 ]
 display(screen)
 save_extension(screen, 'img.png')
 
-#start (0,0); end (10,9)
-draw_line(0 , 0 , 10, 9, screen, color)
+#for i in range(0, 500, 2):
+ #       draw_line(i , i , 500, 500, screen, color)
+
+p0x= 1
+p0y= 1
+p1x= 4
+p1y= 1
+p2x= 4
+p2y= 5
+
+#make triangles
+for i in range(5):
+    draw_line(p0x, p0y, p1x, p1y, screen, color)
+    draw_line(p1x, p1y, p2x, p2y, screen, color) 
+    draw_line(p2x, p2y, p0x, p0y, screen, color)
+    p0x+=6
+    p0y+=6
+    p1x+=6
+    p1y+=6
+    p2x+=6
+    p2y+=6
+
+    
+
+
